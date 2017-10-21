939f68081c33af328d6a1d39e6b32b60ae6b9104
Manivannan Ramarajyam <manivannan.ramarajyam@trinet.com>
2017-10-20 23:44:04 -0700

    Additional Response added for BOLD.

--- src/main/java/com/storepoints/web/GetQuoteAPIController.java
+++ src/main/java/com/storepoints/web/GetQuoteAPIController.java
@@ -15,6 +15,65 @@ public class GetQuoteAPIController {
 	private static int counter=0;
 	
 	
+	private static String getBOLD_QuoteResponse(){
+		return "<QuoteResponse>"
+				+"<QuoteData>"
+				+"  <all>"
+			      +"<adjNonAdjFlag>false</adjNonAdjFlag>"
+			      +"<annualDividend>0.0</annualDividend>"
+			      +"<ask>"+ask_counter+"</ask>"
+			      +"<askExchange>V</askExchange>"
+			      +"<askSize>1</askSize>"
+			      +"<askTime>08:22:29 EDT 09-18-2017</askTime>"
+			      +"<bid>"+bid_counter+"</bid>"
+			      +"<bidExchange>V</bidExchange>"
+			      +"<bidSize>1</bidSize>"
+			      +"<bidTime>08:22:29 EDT 09-18-2017</bidTime>"
+			      +"<chgClose>5.0</chgClose>"
+			      +"<chgClosePrcn>"+chgClosePrcn_counter+"</chgClosePrcn>"
+			      +"<companyName>Audentes Therapeutics Inc</companyName>"
+			      +"<daysToExpiration>0</daysToExpiration>"
+			      +"<dirLast>D</dirLast>"
+			      +"<dividend>0.0</dividend>"
+			      +"<eps>-2.0</eps>"
+			      +"<estEarnings>0.0</estEarnings>"
+			      +"<exDivDate></exDivDate>"
+			      +"<exchgLastTrade>NASDAQ BB Foreign</exchgLastTrade>"
+			      +"<fsi> </fsi>"
+			      +"<high>0.0</high>"
+			      +"<high52>33.43</high52>"
+			      +"<highAsk>24.98</highAsk>"
+			      +"<highBid>33.43</highBid>"
+			      +"<lastTrade>33.43</lastTrade>"
+			      +"<low>0.0</low>"
+			      +"<low52>13.13</low52>"
+			      +"<lowAsk>25.52</lowAsk>"
+			      +"<lowBid>24.94</lowBid>"
+			      +"<numTrades>0</numTrades>"
+			      +"<open>0.0</open>"
+			      +"<openInterest>0</openInterest>"
+			      +"<optionStyle></optionStyle>"
+			      +"<optionUnderlier></optionUnderlier>"
+			      +"<prevClose>24.98</prevClose>"
+			      +"<prevDayVolume>161301</prevDayVolume>"
+			      +"<primaryExchange>u </primaryExchange>"
+			      +"<symbolDesc>Audentes Therapeutics Inc</symbolDesc>"
+			      +"<todayClose>0.0</todayClose>"
+			      +"<totalVolume>0</totalVolume>"
+			      +"<upc>0</upc>"
+			      +"<volume10Day>122579</volume10Day>"
+			    +"</all>"
+			    +"<dateTime>16:26:23 EDT 09-18-2017</dateTime>"
+			    +"<product>"
+			    +"  <symbol>BOLD</symbol>"
+			    +"  <type>EQ</type>"
+			    +"  <exchange>u </exchange>"
+			    +"</product>"
+			  +"</QuoteData>"
+			  + "</QuoteResponse>";
+	}
+	
+	
 	private static String getGBTC_QuoteResponse(){
 		return "<QuoteResponse>"
 				+"<QuoteData>"
@@ -71,7 +130,7 @@ public class GetQuoteAPIController {
 			    +"</product>"
 			  +"</QuoteData>"
 			  + "</QuoteResponse>";
-	}
+	}	
 	
 	
     @RequestMapping("/market/rest/quote/GBTC")
@@ -90,36 +149,57 @@ public class GetQuoteAPIController {
 //    	}
     	
     	// Positive trend    	
-//    	chgClosePrcn_counter=chgClosePrcn_counter+1.0D;
-//    	
-//    	if(chgClosePrcn_counter>= 6.0D){
-//    		bid_counter= bid_counter+5.0;
-//    		ask_counter= ask_counter+5.0;
-//    	}
+    	chgClosePrcn_counter=chgClosePrcn_counter+1.0D;
+    	
+    	if(chgClosePrcn_counter>= 6.0D){
+    		bid_counter= bid_counter+5.0;
+    		ask_counter= ask_counter+5.0;
+    	}
     	
     	//Positive and Negative trend both
-    	if(++counter<10){
-    		if(counter%2==0)
-    			chgClosePrcn_counter=chgClosePrcn_counter-0.75D;
-    		else
-    			chgClosePrcn_counter=chgClosePrcn_counter+1.0D;
-    	}
-    	else
-    		chgClosePrcn_counter=chgClosePrcn_counter-1.0D;
-    	
-    	if(chgClosePrcn_counter>= 6.0D && chgClosePrcn_counter<=10.0d && counter<10){
-    		bid_counter= bid_counter+5.0D;
-    		ask_counter= ask_counter+5.0D;
-    	} else {
-    		bid_counter= bid_counter-5.0D;
-    		ask_counter= ask_counter-5.0D;
-    		
-    	}
-    	
+//    	if(++counter<10){
+//    		if(counter%2==0)
+//    			chgClosePrcn_counter=chgClosePrcn_counter-2.75D;
+//    		else
+//    			chgClosePrcn_counter=chgClosePrcn_counter+1.0D;
+//    	}
+//    	else{
+//    		if(counter%2==0)
+//    			chgClosePrcn_counter=chgClosePrcn_counter+1.0D;
+//    		else
+//    			chgClosePrcn_counter=chgClosePrcn_counter-2.75D;
+//    	}
+//    	
+//    	if(chgClosePrcn_counter>= 6.0D && chgClosePrcn_counter<=10.0d && counter<10){
+//    		bid_counter= bid_counter+5.0D;
+//    		ask_counter= ask_counter+5.0D;
+//    	} else {
+//    		bid_counter= bid_counter-5.0D;
+//    		ask_counter= ask_counter-5.0D;
+//    		
+//    	}
+//    	
     	
     	
     	
     	return getGBTC_QuoteResponse();
     }
+    
+    
+    @RequestMapping("/market/rest/quote/BOLD")
+    public String getQuoteResponseForBOLD() {
+    	
+    	
+    	// Positive trend    	
+    	chgClosePrcn_counter=chgClosePrcn_counter+1.0D;
+    	
+    	if(chgClosePrcn_counter>= 6.0D){
+    		bid_counter= bid_counter+5.0;
+    		ask_counter= ask_counter+5.0;
+    	}
+    	
+    	
+    	return getBOLD_QuoteResponse();
+    }    
 
 }
